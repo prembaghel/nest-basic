518fcdd5f0d41091093cd4d72c3ce0070f26b3b3
Prem Baghel <prem_baghel2002@yahoo.com>
2022-08-22 20:35:02 +0530

    added mysql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactsModule } from './contacts/contacts.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ContactsModule],
+  imports: [
+    ContactsModule,
+    TypeOrmModule.forRoot({
+      //type: 'sqlite',
+      type: 'mysql',
+      host: 'awseb-e-ck3n9vav3v-stack-awsebrdsdatabase-v2cpjxmmsrgn.cuxcfcofxlzj.ap-south-1.rds.amazonaws.com',
+      port: 3306,
+      username: 'premBaghel',
+      password: 'ghantya123$',
+      database: 'employee',
+      dropSchema: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      debug: true,
+      logging: true,
+   }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
 import { ContactsController } from './contacts.controller';
+import { Contact } from './contact.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Contact]),
+  ],
   providers: [ContactsService],
   controllers: [ContactsController]
 })
